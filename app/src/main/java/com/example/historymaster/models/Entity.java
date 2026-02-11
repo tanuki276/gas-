ead66d699bf9387e9009ee1125d04c4753dd34a0
blowa <light06120602@gmail.com>
2026-02-11 20:05:31 +0900

    Entity.java の作成

---
+++ app/src/main/java/com/example/historymaster/models/Entity.java
@@ -0,0 +1,24 @@
+// app/src/main/java/com/historymaster/models/Entity.java
+package com.historymaster.models;
+
+import java.util.List;
+
+public class Entity {
+    private String name;
+    private List<String> tags;
+    private List<Entity> sub_entities;
+    
+    public Entity() {}
+    
+    // ゲッター
+    public String getName() { return name; }
+    public List<String> getTags() { return tags; }
+    public List<Entity> getSubEntities() { return sub_entities; }
+    
+    // セッター
+    public void setName(String name) { this.name = name; }
+    public void setTags(List<String> tags) { this.tags = tags; }
+    public void setSubEntities(List<Entity> sub_entities) { 
+        this.sub_entities = sub_entities; 
+    }
+}
